779a8a25ea6df64024b51567db9b0dadc2704faf
chrvnixx <emmanueladekola91@gmail.com>
2026-05-07 19:33:12 +0100

    get products controller

--- backend/src/controllers/productControllers.js
+++ backend/src/controllers/productControllers.js
@@ -1,3 +1,16 @@
+import Product from "../models/Product";
+
 export async function getProducts(req,res){
-    
+    try {
+       const products = await Product.find({}) 
+
+       if(!products){
+        return res.status(404).json({message:"No products available"})
+       }
+
+       res.status(200).json(products)
+    } catch (error) {
+        res.status(500).json({message:"Internal server error"})
+        console.log("Error in get products controller")
+    }
 }
